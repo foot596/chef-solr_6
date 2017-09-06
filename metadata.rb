5c8c4741b382b683fba69d03abd7a112425047f7
Jeffrey Wong <jwong@dayspring-tech.com>
2017-09-06 11:03:21 -0700

    remove metadata attributes incompatible with chef 11

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ license 'MIT'
 description 'Installs/Configures Apache Solr Version 6'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
-source_url 'https://github.com/ECHOInternational/chef-solr_6'
-issues_url 'https://github.com/ECHOInternational/chef-solr_6/issues'
 
 supports 'amazon'
 supports 'centos'
